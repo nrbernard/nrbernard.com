3264fbb54702e8104d70a966f4ca7aa41a816c22
Nick Bernard <nrobertbernard@gmail.com>
2022-08-07 10:53:16 -0700

    Update homepage (#24)
    * Update homepage
    
    * Remove test
    
    * Skip test in CI

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,9 @@ export default function Index(): JSX.Element {
 	return (
 		<ThemeProvider theme={customTheme}>
 			<Head>
-				<title>Say Their Names</title>
-				<meta
-					name="description"
-					content="Justice for George Floyd. Justice for Breonna Taylor. Justice for Ahmaud Arbery."
-				/>
-				<link rel="icon" href="blm.png"></link>
+				<title>Nick Bernard</title>
+				<meta name="description" content="if ever then always" />
+				<link rel="icon" href="waves.png"></link>
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var t=analytics.methods[e];analytics[t]=analytics.factory(t)}analytics.load=function(e,t){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+e+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=t};analytics.SNIPPET_VERSION="4.1.0";}}();`,
